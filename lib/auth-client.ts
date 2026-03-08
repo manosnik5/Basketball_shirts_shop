248e9cbc8e73951dce607450ff81f3c76defc0c9
Manos <manosnikitakis3@gmail.com>
2026-03-08 14:43:12 +0200

    exported authClient

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,12 +1,14 @@
 import { createAuthClient } from "better-auth/react";
-import {inferAdditionalFields, adminClient} from "better-auth/client/plugins";
+import {inferAdditionalFields, adminClient } from "better-auth/client/plugins";
 import type { auth } from "@/lib/auth"
 import { roles, ac } from "./permissions";
 
 
-const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_API_UR,
-    plugins: [inferAdditionalFields<typeof auth>(), adminClient({ ac, roles })],
+export const authClient = createAuthClient({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    plugins: [
+        inferAdditionalFields<typeof auth>(), adminClient({ ac, roles }),
+    ],
 });
 
-export const { signUp, signOut, signIn, useSession, sendVerificationEmail, forgetPassword, resetPassword, admin } = authClient;
+export const { signUp, signOut, signIn, useSession, sendVerificationEmail, forgetPassword, resetPassword, admin} = authClient;
