6fc639864f5cab9aa46db9e76910d99935685681
Manos <manosnikitakis3@gmail.com>
2026-03-08 12:19:55 +0200

    changed ui

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,7 +1,7 @@
 import ForgotPassword from "@/components/ForgotPassword"
 const page =  () => {
   return (
-    <div className='flex items-center justify-center space-y-4 flex-col h-[70vh]'>
+    <div className='flex items-center justify-center space-y-4 flex-col h-[70vh] not-even:bg-light-light rounded-lg p-8 shadow-shadow-m text-center'>
         <h1 className='text-heading-3'>Password Reset</h1>
         <p className='text-muted-foreground'>
            Please enter your email address to receive a password reset link.
