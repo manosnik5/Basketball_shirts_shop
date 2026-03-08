c4b66e14755f6afe6c2331ec082755986d51dd10
Manos <manosnikitakis3@gmail.com>
2026-03-08 12:47:05 +0200

    changed teamLogoUrl to also be null

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 interface AddToCartButtonProps {
-  teamLogoUrl: string;
+  teamLogoUrl: string | null;  
   shirtName: string;
   variantId: string;
   quantity?: number;
