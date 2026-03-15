32298ed1280dc95a84aa471b5fc7b8ebad6e2fe2
Manos <manosnikitakis3@gmail.com>
2026-03-15 15:09:39 +0200

    fix url primsa 7 error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,8 @@
 import { PrismaClient } from "@/lib/generated/prisma"
-import { PrismaNeon } from "@prisma/adapter-neon";
 
-
-const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ adapter });
+  globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
